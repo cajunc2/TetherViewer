42cfee10e9d5c09f7456b14ac7bd89cab0de1324
Matthew Martin <cajunc2@gmail.com>
2021-03-16 15:22:09 -0400

    Initial Commit

---
+++ TetherViewer/TetherViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetherViewerApp.swift
+//  TetherViewer
+//
+//  Created by Matthew Martin on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TetherViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
